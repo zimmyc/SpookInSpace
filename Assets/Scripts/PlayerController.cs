bd091ddf2615d590139d33570f9ad68cc1e8acf4
Lianardo-bit <honeyshy.gogodash@gmail.com>
2024-12-08 13:54:58 +0000

    Player can flip and slip on the platforms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,13 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveInput;
     private bool isGrounded;
     private Animator animator;
+    private SpriteRenderer spriteRenderer; // Added SpriteRenderer
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Initialize SpriteRenderer
         rb.constraints = RigidbodyConstraints2D.FreezeRotation; 
         // Freeze rotation to prevent tipping
     }
@@ -31,12 +33,23 @@ public class PlayerController : MonoBehaviour
         
         // Update the Speed parameter in the Animator
         animator.SetFloat("Speed", Mathf.Abs(moveInput.x));
+
+        // Flip the sprite based on movement direction
+        if (moveInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
     }
+
     void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
