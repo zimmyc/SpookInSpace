1a6d88f85e751dc9ff71969ecebfe2df197cbe53
zimmyc <nazimchoudry786@gmail.com>
2024-12-13 00:18:19 +0000

    projectiles done
    projectiles work and spawn video scare. should occur during third level cant test since it breaks for me but it shoullddddd work

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,10 +9,15 @@ public class Projectile : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Play the video
-            videoPlayer.Play();
-            // Destroy the projectile
+            VideoPlayer playerVideo = other.gameObject.GetComponent<VideoPlayer>();
+            playerVideo.Play();
+            playerVideo.loopPointReached += EndReached; // Add this line to handle the end of the video
             Destroy(gameObject);
         }
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        vp.Stop(); // This will stop the video when it reaches the end
     }
 }
